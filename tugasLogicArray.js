777f729c194c65bfb67d047dba17a551eaa9091d
Dedyyoga <08776215637.ky@gmail.com>
2021-07-30 21:07:02 +0800

    agak kesusahan nomer 3 ama 4

---
+++ tugasLogicArray.js
@@ -0,0 +1,79 @@
+
+
+// No 1
+// let number = [1,2,3];
+// let total = 0
+// for(let i = 0; i < number.length; i++){
+//     total += number[i];
+// }
+// console.log(total);
+
+
+
+
+
+// No 2
+// function reverseArray(array1) {
+//     var newArray = [];
+//     let array = 0;
+//     for (var i = array1.length -1; i >= 0; i--) {
+//         let arr = array1[i].toString();
+//         let kebalikan ="";
+//         for (let j = arr.length -1; j >= 0; j--){
+//             kebalikan += arr[j];
+            
+//         }
+//         newArray[array] = Number(kebalikan);
+//         array++;
+
+//     }
+//     return newArray;
+//   }
+//   console.log(reverseArray([123,456,789101112]))
+
+// untuk line 26 sama 27 di pindahin ke line 24 jadilah asterik loop dalam array (ga sengaja nemu) tolong bisa dijelasin knp
+
+
+// No 3
+
+// function uniQue (array){
+//     let arr = [987] 
+//     for (let i = 1; i < array.length; i++){
+//         temp = 1;
+//         for (let j = 0; j < arr.length; j++){
+//             if (array[i] == arr[j]){
+//                 temp = 0;
+//                 break;
+//             }
+//         }
+//            if (temp){
+//             arr.push(array[i])
+//         }
+//     }
+//     return arr
+// }
+// console.log(uniQue([1222]))
+// maaf kak saya belum ngerti caranya
+
+
+
+
+
+
+
+
+
+
+
+// No 4
+// maaf saya belum sempat kerjain karena saya tidak terlalu mengerti cara nya.
+
+
+
+
+
+
+
+
+
+
